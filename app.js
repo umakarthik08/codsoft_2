7d6e6e18a5804f8d9a792a893dfb38ab16ec8f0b
UmaKarthik <umakarthikray@gmail.com>
2024-06-30 16:54:21 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const rct = () => {
+    const node = document.getElementById("cta-button")
+    alert("Booked a interview and our mentor will reach u there")
+    node.innerHTML = 'booked successfully'
+}
